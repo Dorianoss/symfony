ddbbc7730379ab19396d12c9b04ba9e4b6a21a80
Denis <ferdinatrastet@gmail.com>
2018-09-08 18:06:48 +0300

    added UserServiceTest

--- tests/Entity/PostTest.php
+++ tests/Entity/PostTest.php
@@ -1,7 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: doriano
- * Date: 05.09.18
- * Time: 19:41
- */
+
+namespace App\tests\Entity;
+
+use App\Entity\Post;
+
+use PHPUnit\Framework\TestCase;
+
+class PostTest extends TestCase
+{
+    public function testGetSetTitle()
+    {
+        $post = new Post();
+        $result = $post->setTitle("12345");
+
+        // assert that your calculator added the numbers correctly!
+        $this->assertEquals("12345", $result->getTitle());
+    }
+}
--- tests/Service/UserServiceTest.php
+++ tests/Service/UserServiceTest.php
@@ -1,7 +1,49 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: doriano
- * Date: 05.09.18
- * Time: 17:16
- */
+
+use App\Entity\User;
+use App\Repository\UsersRepository;
+use App\Service\UserService;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+
+class UserServiceTest extends TestCase
+{
+    public function testRegister()
+    {
+        $user = $this->prophesize(User::class);
+        $user->getEmail()->willReturn('ferdinatrastet@gmail.com')->shouldBeCalled();
+
+        $repo = $this->prophesize(UsersRepository::class);
+        $repo->findOneBy(['email' => 'ferdinatrastet@gmail.com'])->shouldBeCalled();
+
+        $em=$this->prophesize(EntityManagerInterface::class);
+        $em->getRepository(User::class)->willReturn($repo->reveal())->shouldBeCalled();
+        $em->persist(Argument::type(User::class))->shouldBeCalled();
+        $em->flush()->shouldBeCalled();
+
+        $userService = new UserService($em->reveal());
+        $registeredUser = $userService->register($user->reveal());
+
+        $this->assertInstanceOf(User::class, $registeredUser);
+    }
+
+    public function testRegisterException()
+    {
+        $user = $this->prophesize(User::class);
+        $user->getEmail()->willReturn('ferdinatrastet@gmail.com')->shouldBeCalled();
+
+        $repo = $this->prophesize(UsersRepository::class);
+        $repo->findOneBy(['email' => 'ferdinatrastet@gmail.com'])->willReturn($user->reveal())->shouldBeCalled();
+
+        $em=$this->prophesize(EntityManagerInterface::class);
+        $em->getRepository(User::class)->willReturn($repo->reveal())->shouldBeCalled();
+        $em->persist(Argument::type(User::class));
+
+        $userService = new UserService($em->reveal());
+        try {
+            $registeredUser = $userService->register($user->reveal());
+            $this->assertInstanceOf(User::class, $registeredUser);
+        } catch (Exception $e){}
+    }
+}
