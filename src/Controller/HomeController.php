80aa63be16d9f3bbfc87da059929fca21b08e467
Denis <ferdinatrastet@gmail.com>
2018-09-22 18:07:04 +0300

    added VkApi, /home path returns friends list

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,24 +3,34 @@
 namespace App\Controller;
 
 use App\Service\VkAPI;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use VK\Client\VKApiClient;
 
 class HomeController extends Controller
 {
     /**
      * @Route("/home", name="home")
      * @param VkAPI $vkAPI
+     * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
-     * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function indexAction(VkAPI $vkAPI)
+    public function indexAction(VkAPI $vkAPI, Request $request)
     {
-        $friend=$vkAPI->getFriends();
-        dump($friend);
-        die();
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        $code = $request->get('code');
+        if (!$code) {
+            $authorize = $vkAPI->authorize();
+            header('Location: ' . $authorize);
+        }
+        else{
+            $token = $vkAPI->getToken($code);
+            $friends = $vkAPI->getFriends($token);
+            dump($friends);
+        }
+        die;
+//        return $this->render('home/index.html.twig', [
+//            'controller_name' => 'HomeController',
+//        ]);
     }
 }
--- src/Service/VkAPI.php
+++ src/Service/VkAPI.php
@@ -1,32 +1,50 @@
 <?php
 
+#    env(VK_URL): ''
+#    env(VK_CLIENT): ''
+#    env(VK_SECRET): ''
+#    env(VK_OAUTH_URL): ''
+
 namespace App\Service;
 
-use GuzzleHttp\Client;
+use VK\Client\VKApiClient;
+use VK\OAuth\Scopes\VKOAuthUserScope;
+use VK\OAuth\VKOAuth;
+use VK\OAuth\VKOAuthDisplay;
+use VK\OAuth\VKOAuthResponseType;
 
 class VkAPI
 {
-    /**
-     * @var Client
-     */
-    private $client;
 
-    /**
-     * VkAPI constructor.
-     * @param Client $client
-     */
-    public function __construct(Client $client)
+    public function authorize()
     {
-        $this->client = $client;
+        $oauth = new VKOAuth();
+        $client_id = 6699413;
+        $redirect_uri = 'http://127.0.0.1:8001/home';
+        $display = VKOAuthDisplay::PAGE;
+        $scope = array(VKOAuthUserScope::FRIENDS, VKOAuthUserScope::PHOTOS, VKOAuthUserScope::VIDEO);
+        $state = 'secret_state_code';
+
+        return $browser_url = $oauth->getAuthorizeUrl(VKOAuthResponseType::CODE,
+            $client_id, $redirect_uri, $display, $scope, $state);
+
+//        return json_decode($this->client->request('GET', '/method/friends.get')->getBody()->getContents(), true);return json_decode($this->request('GET', '/method/friends.get')->getBody()->getContents(), true);
     }
 
-
-    /**
-     * @return array
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    public function getFriends():array
+    public function getToken($code)
     {
-        return json_decode($this->client->request('GET', '/method/friends.get')->getBody()->getContents(), true);return json_decode($this->request('GET', '/method/friends.get')->getBody()->getContents(), true);
+        $oauth = new VKOAuth();
+        $client_id = 6699413;
+        $client_secret = 'nhoRcQSIyzt4KwROC6B0';
+        $redirect_uri = 'http://127.0.0.1:8001/home';
+
+$response = $oauth->getAccessToken($client_id, $client_secret, $redirect_uri, $code);
+return $access_token = $response['access_token'];
+    }
+
+    public function getFriends($access_token)
+    {
+        $vk = new VKApiClient();
+        return $response = $vk->friends()->get($access_token);
     }
 }
